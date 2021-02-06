833a9818e08cbaed0509ce29d1c96431b79c1a26
JuliuszG <juliusz.goral@gmail.com>
2021-02-06 16:47:35 +0100

    nav 2.11

--- src/components/navigation/nav2/menuDesktop.jsx
+++ src/components/navigation/nav2/menuDesktop.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { motion } from "framer-motion"
 import { colors } from '../../../styles/colors'
@@ -14,6 +14,10 @@ const Style = styled(motion.div)`
     position: fixed;
     top: 0;
     left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
 `
 const menuAnimation = {
     visible: { 
@@ -48,19 +52,50 @@ const itemAnimation = {
     },
     hidden: { 
         x: -300, 
-        y: 100, 
         opacity: 0,
     },
-    exit: {
-        x: -300,
-        opacity: 0
-    }
+}
+
+const subItemAnimation = {
+    visible: i => ({ 
+        x: 0, 
+        opacity: 1,
+        transition: {
+            delay: i * 0.1,
+            ease: "easeIn", 
+            duration: 0.2
+        }
+    }),
+    hidden: { 
+        x: 200, 
+        opacity: 0,
+    },
+}
+
+const lineAnimation = {
+    visible: { 
+        width: "5%",
+        opacity: 1,
+        transition: {
+            ease: "easeIn", 
+            duration: 0.2
+        }
+    },
+    hidden: { 
+        width: 0,
+        opacity: 0,
+        transition: {
+            ease: "easeIn", 
+            duration: 0.1
+        }
+    },
 }
 
 const HeaderStyle = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    width: 100%;
     padding: 3%;
     .cnt {
         width: 367px;
@@ -98,12 +133,22 @@ export const Exit = styled.div`
     }
 `
 const Links = styled.div`
-    padding: 5% 10%;
+    padding: 0 10%;
     width: 100%;
+    display: flex;
+    @media (max-width: 1650px) {
+        font-size: 12px;
+    }
+    @media (max-width: 1050px) {
+        font-size: 10px;
+    }
+    @media (width: 1024px) and (height: 1366px) {
+        font-size: 16px;
+    }
     .main {
+        width: 40em;
         font-family: 'Poppins';
         font-style: normal;
-        border: 1px solid white;
         .item {
             cursor: pointer;
             display: block;
@@ -114,6 +159,7 @@ const Links = styled.div`
             line-height: 272.72%;
             display: flex;
             a {
+                display: block;
                 text-decoration: none;
                 color: ${ colors.neutral00 };
             }
@@ -128,13 +174,109 @@ const Links = styled.div`
         }
     }
     .line {
-        width: 10%;
+        display: block;
+        width: 0%;
         height: 1px;
         background: ${ colors.neutral00 };
-        margin-left: 10%;
+        margin-left: -12em;
+        margin-top: 12em;
+    }
+    .sub-menu {
+        width: 30%;
+        font-family: 'Poppins';
+        font-style: normal;
+        margin-top: 10em;
+        margin-left: 8em;
+        .sub-item {
+            display: block;
+            a {
+                text-decoration: none;
+                color: ${ colors.neutral00 };
+                display: block;
+                font-weight: 600;
+                font-size: 1.5625em;
+                line-height: 244%;
+            }
+        }
+    }
+`
+
+const MenuFooter = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    padding: 0 10%;
+    font-family: 'Poppins';
+    font-style: normal;
+    color: ${ colors.neutral00 };
+    .social {
+        display: flex;
+        .row {
+            flex-grow: 1;
+            &:first-of-type {
+                margin-right: 12em;
+            }
+            a {
+                display: block;
+                color: ${ colors.neutral00 };
+                text-decoration: none;
+                font-weight: 300;
+                font-size: 1.0625em;
+                line-height: 264.70%;
+            }
+        }
+    }
+    .contact {
+        position: relative;
+        @media (max-width: 1650px) {
+            font-size: 12px;
+        }
+        @media (max-width: 1050px) {
+            font-size: 10px;
+        }
+        bottom: 5em;
+        h3 {
+            font-weight: 600;
+            font-size: 1.5625em;
+            line-height: 480%;
+        }
+        a {
+            display: block;
+            color: ${ colors.neutral00 };
+            text-decoration: none;
+            font-weight: 300;
+            font-size: 1.1875em;
+            line-height: 168.42%;
+        }
+    }
+`
+
+const Copy = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    @media (max-width: 1650px) {
+        font-size: 12px;
+    }
+    @media (max-width: 1050px) {
+        font-size: 10px;
+    }
+    p {
+        color: ${ colors.neutral30 };
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 300;
+        font-size: 0.9375em;
+        line-height: 386.66%;
     }
 `
 const MenuDesktop = ({ toggle }) => {
+    const [subMenuIsOn, setSubMenuIsOn] = useState(false)
+    const menuSwitch = () => {
+        toggle()
+        setSubMenuIsOn(false)
+    }
     return (
             <Style
                 variants={ menuAnimation }
@@ -144,7 +286,7 @@ const MenuDesktop = ({ toggle }) => {
                 key="efwefwweffewfefewf"
             >
                 <HeaderStyle>
-                    <Exit onClick={ toggle } color={ colors.neutral00 }>
+                    <Exit onClick={ menuSwitch } color={ colors.neutral00 }>
                         <span className="sr" role="button">exit button</span>
                     </Exit>
                     <div className="cnt">
@@ -154,7 +296,7 @@ const MenuDesktop = ({ toggle }) => {
                 </HeaderStyle>
                 <Links>
                     <div className="main">
-                        <motion.div variants={ itemAnimation } key="efwefew" className="item">
+                        <motion.div variants={ itemAnimation } className="item">
                             <div className="number">
                                 01.
                             </div>
@@ -162,7 +304,11 @@ const MenuDesktop = ({ toggle }) => {
                                 Strona głowna
                             </Link>
                         </motion.div>
-                        <motion.div variants={ itemAnimation } className="item">
+                        <motion.div 
+                            variants={ itemAnimation } 
+                            className="item" 
+                            onClick={ () => setSubMenuIsOn(prevState => !prevState) }
+                        >
                             <div className="number">
                                 02.
                             </div>
@@ -174,7 +320,7 @@ const MenuDesktop = ({ toggle }) => {
                             <div className="number">
                                 03.
                             </div>
-                            <ScLink to={ "caseStudy" } smooth duration={1000} onClick={ toggle }>
+                            <ScLink to={ "caseStudy" } smooth duration={1000} onClick={ menuSwitch }>
                                 Nasze prace 
                             </ScLink>
                         </motion.div>
@@ -182,13 +328,78 @@ const MenuDesktop = ({ toggle }) => {
                             <div className="number">
                                 04.
                             </div>
-                            <ScLink to={ "footer" } smooth duration={1000} onClick={ toggle }>
+                            <ScLink to={ "footer" } smooth duration={1000} onClick={ menuSwitch }>
                                 Kontakt
                             </ScLink>
                         </motion.div>
                     </div>
+                    <motion.div 
+                        variants={ lineAnimation } 
+                        animate={ subMenuIsOn ? "visible" : "hidden"}
+                        className="line"
+                    ></motion.div>
+                    { subMenuIsOn && (
+                    <div className="sub-menu">
+                        <motion.div 
+                            className="sub-item"
+                            variants={ subItemAnimation }
+                            custom={ 1 }
+                        >
+                            <Link to="/branding">Branding</Link>
+                        </motion.div>
+                        <motion.div 
+                            className="sub-item"
+                            variants={ subItemAnimation }
+                            custom={ 2 }
+                        >
+                            <Link to="/ecommerce">eCommerce</Link>
+                        </motion.div>
+                        <motion.div 
+                            className="sub-item"
+                            variants={ subItemAnimation }
+                            custom={ 3 }
+                        >
+                            <Link to="/events">Events</Link>
+                        </motion.div>
+                        <motion.div 
+                            className="sub-item"
+                            variants={ subItemAnimation }
+                            custom={ 4 }
+                        >
+                            <Link to="/marketing">Marketing </Link>
+                        </motion.div>
+                        <motion.div 
+                            className="sub-item"
+                            variants={ subItemAnimation }
+                            custom={ 5 }
+                        >
+                            <Link to="/development">Development </Link>
+                        </motion.div>
+                    </div>
+    )  }
                 </Links>
-                <div className="line"></div>
+                <div style={{ width: "100%" }}>
+                    <MenuFooter>
+                        <div className="social">
+                            <div className="row">
+                                <a href="http://www.facebook.pl">Facebook</a>
+                                <a href="http://www.dribble.com">Dribble</a>
+                            </div>
+                            <div className="row">
+                                <a href="http://www.instagram.com">Instagram</a>
+                                <a href="http://www.linkedin.com">Linkedin</a>
+                            </div>
+                        </div>
+                        <div className="contact">
+                            <h3>
+                                Skontaktuj się z nami
+                            </h3>
+                            <a href="tel:+48881772030">+48 881 772 030</a>
+                            <a href="mailto:we@explayn.it">we@explayn.it</a>
+                        </div>
+                    </MenuFooter>
+                    <Copy><p>© explayn.it All rights reserved</p></Copy>
+                </div>
             </Style>
     )
 }
