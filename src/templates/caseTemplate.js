9adac8bc028520ec10f157101c6cd834f0e2eb44
JuliuszG <juliusz.goral@gmail.com>
2021-03-06 17:06:08 +0100

    icons anim8

--- src/templates/caseTemplate.js
+++ src/templates/caseTemplate.js
@@ -20,7 +20,7 @@ query MyQuery($id: String!) {
     datoCmsRealizacja(id: {eq: $id}) {
         landingimage {
           fluid(maxWidth: 1750) {
-            ...GatsbyDatoCmsFixed_tracedSVG
+            ...GatsbyDatoCmsFluid_tracedSVG
           }
         }
         component2Title
@@ -32,24 +32,24 @@ query MyQuery($id: String!) {
         link
         screens {
           fluid(maxWidth: 1750) {
-            ...GatsbyDatoCmsFixed_tracedSVG
+            ...GatsbyDatoCmsFluid_tracedSVG
           }
         }
         bigScreen {
           fluid(maxWidth: 1750) {
-            ...GatsbyDatoCmsFixed_tracedSVG
+            ...GatsbyDatoCmsFluid_tracedSVG
           }
         }
         component4Title
         article4Paragraphs
         doubleImageComponent {
           fluid(maxWidth: 1750) {
-            ...GatsbyDatoCmsFixed_tracedSVG
+            ...GatsbyDatoCmsFluid_tracedSVG
           }
         }
         screen2 {
           fluid(maxWidth: 1750) {
-            ...GatsbyDatoCmsFixed_tracedSVG
+            ...GatsbyDatoCmsFluid_tracedSVG
           }
         }
         component7Title
