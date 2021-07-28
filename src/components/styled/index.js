122909d666878d053fb5794cba8ae4aaec84af0f
Gaweł <gaw.gawel@gmail.com>
2021-07-28 13:14:36 +0200

    fix: move thanks card styled components

--- src/components/styled/index.js
+++ src/components/styled/index.js
@@ -1,7 +1,8 @@
 import styled from "styled-components"
 import Image from "gatsby-image"
-import { colors } from "../../styles/colors"
 import BackgroundImage from "gatsby-background-image"
+import { motion } from "framer-motion"
+import { colors } from "../../styles/colors"
 
 //blog/author.jsx
 export const AuthorWrapper = styled.div`
@@ -795,5 +796,105 @@ export const VideoWrapper = styled.div`
     z-index: 40;
     width: 100%;
     height: 100%;
+  }
+`
+//thankyoucard.jsx
+
+export const ThankYouCardWrapper = styled(motion.div)`
+  width: 100%;
+  height: 100vh;
+  padding: 5% 15%;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font-family: "Poppins", sans-serif;
+  z-index: 999;
+  @media (max-width: 1024px) {
+    font-size: 14px;
+    justify-content: center;
+    align-items: center;
+  }
+  @media (max-width: 500px) {
+    font-size: 12px;
+  }
+  h1 {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 5.25em;
+    letter-spacing: -1.34px;
+    color: #000000;
+    line-height: 110%;
+    margin-bottom: 2.38vh;
+    @media (max-width: 1680px) {
+      width: 75%;
+    }
+    @media (max-width: 1250px) {
+      width: 100%;
+    }
+    @media (max-width: 500px) {
+      font-size: 4em;
+    }
+  }
+  p {
+    font-size: 1.25em;
+    line-height: 145%;
+    width: 50%;
+    @media (max-width: 1024px) {
+      width: 100%;
+    }
+    &:first-of-type {
+      margin-bottom: 2%;
+    }
+  }
+  button {
+  }
+`
+export const ThankYouButton = styled.button`
+  width: 254px;
+  height: 58px;
+  border: 3px solid #5163f6;
+  border-radius: 37px;
+  background: transparent;
+  font-size: 29px;
+  outline: none;
+  cursor: pointer;
+  color: #5163f6;
+  position: relative;
+  margin-top: 5%;
+  transition: color 0.6s ease-in;
+  overflow: hidden;
+  &:hover {
+    color: #fff;
+  }
+  &:hover::after {
+    right: -0.8px;
+  }
+  &:hover p {
+    position: absolute;
+    top: calc(50% - 25px);
+    left: 0%;
+  }
+  p {
+    width: 100%;
+    height: 100%;
+    z-index: 500;
+  }
+  &::after {
+    content: "";
+    display: block;
+    width: 254px;
+    height: 100px;
+    background: #5163f6;
+    position: absolute;
+    right: -254px;
+    top: -3px;
+    transition: right 0.3s ease-in;
+    z-index: 30;
+  }
+  @media (max-width: 1024px) {
+    display: flex;
+    justify-content: center;
+    margin-top: 10%;
   }
 `
