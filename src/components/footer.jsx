86f07960f5e0cb05f40eaea84336b87481388187
Gaweł <gaw.gawel@gmail.com>
2021-07-29 11:46:52 +0200

    fix: add footer redirection

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { Link } from "gatsby"
+import { Link as ScLink } from "react-scroll"
 import Dots from "../images/footer-dots.svg"
 import Arrow from "../images/footer-arrow.svg"
 import emailjs from "emailjs-com"
@@ -87,7 +88,15 @@ const Footer = () => {
         <div className="site-links-column sl2">
           <h3>About</h3>
           <Link to="/">Home</Link>
-          <Link to="/#caseStudy">Case studies</Link>
+          {window.location.pathname === "/" ? (
+            <ScLink to={"caseStudy"} smooth duration={1000}>
+              Case studies
+            </ScLink>
+          ) : (
+            <Link to={"/"} state={{ scrollAnchor: "#caseStudy" }}>
+              Case studies
+            </Link>
+          )}
           <Link to="/blog">Blog</Link>
         </div>
         <div className="site-links-column sl3">
